72bd3c75488156628a0e73f95a35a2e1d030b4ae
surbhir2024 <surbhi.rank.2024@gmail.com>
2024-04-09 12:06:39 +0530

    modified folder structure

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
+connection.connect(function (error) {
+  try {
+    if (error) console.log(error);
+    else console.log("Database Connected!");
+  } catch (error) {
+    console.log(error);
+  }
+  
+});
