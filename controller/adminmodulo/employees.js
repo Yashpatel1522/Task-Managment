676dff7f72f758cf06f880a01942f3e99ad60a93
krushi0912 <krushi.patel.2024.trainee@gmail.com>
2024-04-10 19:04:36 +0530

    admin panel design

--- controller/adminmodulo/employees.js
+++ controller/adminmodulo/employees.js
@@ -1,8 +1,17 @@
+const database = require("../../helpers/database.helper");
 const logger = require("../../logger/logger");
 
-exports.adminemployees = (request,response) => {
+
+let db = new database()
+exports.adminemployees = async(request,response) => {
     try {
-        response.render("adminmodulo/employees")
+        let query = `select u.first_name,u.last_name,u.email,u.contact,u.date_of_birth from users as u left join roles as r 
+        on u.role_id = r.id where r.role_name="Employee";`
+        let result = await db.executeQuery(query);
+        let key = Object.keys(result[0]);
+        console.log(result);
+        console.log(key);
+        response.render("adminmodulo/employees",{result,key})
     } catch (err) {
         logger.error(err);
     }
