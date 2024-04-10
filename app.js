769ade6b6345de881f0ed56a8d502599593ca6a5
RushiF24 <rushikesh.falak.2024@gmail.com>
2024-04-10 19:49:09 +0530

    created design of employee dashboard and controller and route of get dashboard data

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const ejs = require('ejs');
 const logger = require('./logger/logger')
 const adminroute = require('./routers/adminroute')
+const employeeroute = require('./routers/employeeroute')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,8 +17,11 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 let PORT = process.env.PORT;
 
+app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+app.use("/js",express.static("./node_modules/bootstrap/dist/js"));
 
 app.use('/admin', adminroute);
+app.use('/employee', employeeroute);
 // To test logger
 
 // app.get('/', (req, res)=> {
