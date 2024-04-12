9e5ccb780f8fd4a2a9797c81e51f4ee4202fbc45
krushi0912 <krushi.patel.2024.trainee@gmail.com>
2024-04-12 17:16:40 +0530

    employee register form with validation

--- controller/adminmodulo/employees.js
+++ controller/adminmodulo/employees.js
@@ -4,11 +4,16 @@ const logger = require("../../logger/logger");
 
 exports.adminEmployees = async(request,response) => {
     try {
+        let pageno = request.query.p || 1;
+        let limit = 6;
+        let offset = (Number(pageno) - 1) * limit;
         let db = new database();
         let query = `select u.first_name,u.last_name,u.email,u.contact from users as u 
         left join roles as r on u.role_id = r.id where role_name=?`;
-        let employeeData = await db.executeQuery(query,["Employee"]); 
-        response.render("adminmodulo/employees",{employeeData})
+        let result = await db.executeQuery(query,["Employee"]); 
+        let page = Math.ceil(result.length / limit);
+        let employeeData = result.slice(offset, offset + limit);
+        response.render("adminmodulo/employees", { employeeData, pageno,page});
     } catch (err) {
         console.log(err);
         // logger.error(err);
