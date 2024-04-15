abf2aaaec540933f81554d91af088975ab7905a2
Vishvas-Panshiniya <vishvas.panshiniya.2024@gmail.com>
2024-04-15 18:38:42 +0530

    employee and managers data crud operation and pagenation added

--- controller/adminmodulo/managers.js
+++ controller/adminmodulo/managers.js
@@ -1,6 +1,8 @@
 // const getConnection = require("../../config/connection");
+const { response } = require("express");
 const database = require("../../helpers/database.helper");
 const logger = require("../../logger/logger");
+const { request } = require("http");
 
 exports.adminManagers = async (request, response) => {
     try {
@@ -18,10 +20,35 @@ exports.managerDetails = async (request,response) => {
     try {
         let managerId = request.params.id;
         let db = new database()
-        let query = `select users.* from users left join roles on users.role_id = roles.id where users.id = ?`
-        let managerDetail = await db.executeQuery(query, [managerId]);
+        let query = `select users.* from users left join roles on users.role_id = roles.id where role_name = ? and users.id = ?`
+        let managerDetail = await db.executeQuery(query, ["Manager", managerId]);
         return response.json({managerDetail}) 
     } catch (error) {
         logger.log(error)
+    }
+}
+
+exports.searchManData = async (request,response) => {
+    try {
+        let search = request.params.searchdata;
+        search = "%" + search + "%";
+        let db = new database()
+        let query = `select users.* from users left join roles on users.role_id = roles.id where role_name = ? and first_name like ? or last_name like ? `
+        let searchData = await db.executeQuery(query, ["Manager",search, search]);
+        return response.json({searchData}) 
+    } catch (error) {
+        logger.log(error);
+    }
+}
+
+exports.dataDelete = async (request,response) => {
+    try {
+        let id = request.params.id
+        let db = new database();
+        let deletedata = await db.delete("users", {id});
+        return response.json({deletedata})
+    } catch (error) {
+        console.log(error);
+        logger.error(error);
     }
 }
--- routers/employeeroute.js
+++ routers/employeeroute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const { getdashboardata, dashboard } = require("../controller/employeemodule/dashboard")
 
-const { EmployeeTaskList, gettasks, list } = require("../controller/employeemodule/employeetasklist")
+const { EmployeeTaskList, list } = require("../controller/employeemodule/employeetasklist")
 
 router.get('/employeetasklist', EmployeeTaskList)
-router.get('/employeetasklist/:id', gettasks)
+
 router.get('/', list)
 
 router.get("/getdashboardata", getdashboardata)
