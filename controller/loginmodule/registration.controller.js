da9380e1a6e0b84862c5619edb816317559573d1
YashVachhani1522 <yash.vachhani.2024@gmail.com>
2024-04-16 15:21:34 +0530

    create activation code crud in database create multer file uploading store user data in database with status

--- controller/loginmodule/registration.controller.js
+++ controller/loginmodule/registration.controller.js
@@ -1,35 +1,20 @@
-const database = require("../../helpers/database.helper")
-
+const addUser = require("../../services/auth/registration");
 
 const registrationGet=(req,res)=>{
     res.render("loginmodule/registration.ejs")
 }
 
 const registrationPost=async(request,response)=>{
-    console.log(request.body) 
-    let registrationdata={
-        role_id:request.body.role_id,
-        first_name:request.body.first_name,
-        last_name:request.body.last_name,
-        email:request.body.email,
-        contact:request.body.contact,
-        date_of_birth:request.body.date_of_birth,
-        employee_role:request.body.employee_role
-    }   
-    let db=new database()
-    let res=await db.insertData(registrationdata,"users")
-    console.log(request.file)
-    let file=request.file
-    let userprofiledata={
-        "user_id":res.insertId,
-        "oldimage_name":file.originalname,
-        "newimage_name":file.filename,
-        "path":file.path
+    let flag=await addUser(request.body,request.file)
+    if(flag.flag==true)
+    {
+        response.send(flag).status(200)
     }
-    res=await db.insertData(userprofiledata,"user_profiles")
-    console.log(res)
+    else
+    {
+        response.send(flag).status(500)
+    }
 }
-
 const dashboardGet=(req,res)=>{
     res.render("loginmodule/dashboard.ejs")
 }
--- utility/multer.js
+++ utility/multer.js
@@ -3,11 +3,19 @@ const path = require('path');
 
 const userProfileStorage= multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(file)
     cb(null,path.join(__dirname,"../public/assets/userprofiles"))
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname)
   },
 })
-module.exports=userProfileStorage;
+
+const taskdetailfiles=multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null,path.join(__dirname,"../public/assets/taskdetailfiles"))
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+module.exports={userProfileStorage,taskdetailfiles};
