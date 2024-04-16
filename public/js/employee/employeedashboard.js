0c2f7be939384676b6614b3d07e87da185a9a27c
RushiF24 <rushikesh.falak.2024@gmail.com>
2024-04-16 18:37:00 +0530

    loaded data in update profile modal and file using multer

--- public/js/employee/employeedashboard.js
+++ public/js/employee/employeedashboard.js
@@ -50,17 +50,22 @@ const renderData = (dashboardData) => {
 }
 
 const renderProfileData = (profileData) => {
-  // console.log('this is ', profileData);
-  let inputFields = document.querySelectorAll('.form-control')
-  inputFields = Array.from(inputFields)
-  // console.log(inputFields);
-//   Object.keys(profileData[0]).map((val)=>{
-// console.log(val);
-//   })
+  let keys = Object.keys(profileData[0])
+  console.log(profileData[0]);
 
+  keys.map((key)=> {
 
-}
-getDashBoardData("http://localhost:8000/employee/getdashboardata").then((data) => {
+    if(!(document.getElementById(`${key}`) == null)) {
+      document.getElementById(`${key}`).value = profileData[0][key]
+    }
+    else if(key=="employee_role"){
+      document.getElementById(`${key}`).value = profileData[0][key]
+    }
+    })
+    let profileImageName = profileData[0].img_url.split("/")
+    document.getElementById('selectedImage').src = `/assets/userprofiles/${profileData[0].id}/${profileImageName[3]}`
+  }
+getDashBoardData("/employee/getdashboardata").then((data) => {
   dashboardData = data.result
   renderData(dashboardData)
 });
@@ -75,7 +80,7 @@ async function getDashBoardData(url) {
 }
 
 async function loadProfile() {
-  let response = await fetch("http://localhost:8000/employee/getprofiledata").then((response) => { return response.json() }).then((data) => {
+  let response = await fetch("/employee/getprofiledata").then((response) => { return response.json() }).then((data) => {
     profileData = data.result;
     renderProfileData(profileData)
   })
