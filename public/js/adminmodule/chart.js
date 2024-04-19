4cfcda0981cf105e1c8a4fc304b4c499de46f420
krushi0912 <krushi.patel.2024.trainee@gmail.com>
2024-04-19 18:57:42 +0530

    implement chart and activity logs

---
+++ public/js/adminmodule/chart.js
@@ -0,0 +1,45 @@
+let label = [];
+let data = [];
+
+const chartShow = async () => {
+  let result = await (await fetch(`/admin/chartsData`)).json();
+  result.chartData.forEach(e => {
+    label.push(e.label);
+    data.push(e.data);
+  });
+  const Pieadmin = {
+    series: data,
+    chart: {
+      height: 400,
+      width: 400,
+      type: 'pie',
+    },
+    labels: ['Todo', 'Completed', 'In Progress'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  };
+
+  var Piechart = new ApexCharts(document.getElementById("chart"), Pieadmin);
+  console.log(Piechart);
+  Piechart.render();
+  managerAssignTask()
+}
+
+const managerAssignTask = async () => {
+  let task = await(await fetch(`/admin/managerTask`)).json();
+  let TaskActivity = document.getElementById("recentActivity");
+  let logs = ""
+  task.managerAssignTask.forEach(e => {
+    logs += `<p><span class="text-secondary"> ${e.first_name} created </span> <span class = "text-success"> ${e.task_name} </span> </p>`
+  }); 
+  TaskActivity.innerHTML = logs;
+}
