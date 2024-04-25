694b63c16126e434d0de552fe3577ab9274ecd8d
krushi0912 <krushi.patel.2024.trainee@gmail.com>
2024-04-25 11:42:40 +0530

    employee dashboard changed

--- controller/employeemodule/dashboard.js
+++ controller/employeemodule/dashboard.js
@@ -8,15 +8,14 @@ const db = new database();
 const getdashboardata = async (request, response) => {
   try {
 
-    let id = 11;
+    let id = 3;
     let res = {};
-    let employeeTaskStatusCountsQuery = `select count(task_id) as Assigned, count(case when task_status = 'todo' then 1 end) as "To Do",count(case when task_status = 'inprogress' then 1 end) as "In Progress", count(case when task_status = 'completed' then 1 end) as Completed from tasks_assigend_to inner join tasks on tasks_assigend_to.task_id=tasks.id where emp_id = ?;`;
-
+    
     let employeeUpCommingDeadlineQuery = `select task_name as Task, task_end_date as "Due Date", urgency.type as Urgency, importants.type as Importance, task_status as Status from tasks_assigend_to inner join tasks on tasks_assigend_to.task_id=tasks.id inner join priorities on priorities.id = tasks.prioritiy_id inner join urgency on urgency.id = priorities.urgency_id inner join importants on importants.id = priorities.important_id where emp_id = ? and task_end_date >= ? and task_status != 'completed'`;
 
-    let employeeInprogressTaskQuery = `select task_name as Task, task_end_date as "Due Date", urgency.type as Urgency, importants.type as Importance from tasks_assigend_to inner join tasks on tasks_assigend_to.task_id=tasks.id inner join priorities on priorities.id = tasks.prioritiy_id inner join urgency on urgency.id = priorities.urgency_id inner join importants on importants.id = priorities.important_id where emp_id = ? and task_status="inprogress";`;
+    let employeeInprogressTaskQuery = `select task_name as Task, task_end_date as "Due Date" from tasks_assigend_to inner join tasks on tasks_assigend_to.task_id=tasks.id inner join priorities on priorities.id = tasks.prioritiy_id inner join urgency on urgency.id = priorities.urgency_id inner join importants on importants.id = priorities.important_id where emp_id = ? and task_status="inprogress";`;
 
-    res.taskStatusCounts = await db.executeQuery(employeeTaskStatusCountsQuery, [id]);
+    
     res.upCommingDeadlineData = await db.executeQuery(employeeUpCommingDeadlineQuery, [id, currentDate]);
     res.employeeInprogressTaskData = await db.executeQuery(employeeInprogressTaskQuery, [id]);
 
@@ -25,8 +24,11 @@ const getdashboardata = async (request, response) => {
     logger.error("Dash data is not found !");
   }
 };
-const dashboard = (request, response) => {
-  response.render("employeemodule/dashboard");
+
+const dashboard = async (request, response) => {
+  let employeeTaskStatusCountsQuery = `select count(task_id) as Assigned, count(case when task_status = 'todo' then 1 end) as ToDo,count(case when task_status = 'inprogress' then 1 end) as InProgress, count(case when task_status = 'completed' then 1 end) as Completed from tasks_assigend_to inner join tasks on tasks_assigend_to.task_id=tasks.id where emp_id = ?`;
+  [taskStatusCounts] = await db.executeQuery(employeeTaskStatusCountsQuery, 3);
+  response.render("employeemodule/dashboard", { taskStatusCounts });
 };
 
 
