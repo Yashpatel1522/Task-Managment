26d94863a8f62c88ebbea1f67c0618a02b53178e
surbhir2024 <surbhi.rank.2024@gmail.com>
2024-04-30 18:58:33 +0530

    some bug solve

--- routers/managerroute.js
+++ routers/managerroute.js
@@ -57,12 +57,11 @@ const {
 } = require("../controller/managermodule/calender");
 const { reportGet } = require("../controller/employeemodule/reports");
 
-const uploadStorage = multer({ storage: taskdetailfiles });
+// const uploadStorage = multer({ storage: taskdetailfiles });
 const uploadImage = multer({ storage: userProfileStorage });
 
 const managerRouter = express.Router();
 
-// // Displaying Tasks of manager
 managerRouter.get("/calenderData/:month", calenderMonth);
 managerRouter.get("/searchEmploye/:searchdata", searchEmpData);
 
@@ -76,17 +75,12 @@ managerRouter.get(
   passport.authenticate("jwt", { session: false, failureRedirect: "/" }),
   searchTask
 );
-
 managerRouter.get("/getManagerUpcomingTasks", upcomingTasks);
-
 managerRouter.use(
   passport.authenticate("jwt", { session: false, failureRedirect: "/" }),
   checkUserRole
 );
-  
 managerRouter.post("/inserttask", upload.array("files"), inserttaskdata);
-
-// Displaying Employee details
 managerRouter.get("/employeeDetails", employeeView);
 
 managerRouter.get("/Teams", (request, response) => {
@@ -98,22 +92,14 @@ managerRouter.get("/tasks", taskView().getPage);
 // Dashboard
 managerRouter.get("/dashboard", dashboardView);
 managerRouter.post("/inserttask", upload.array("files"), inserttaskdata);
-// API to get team details of the particular manager
 managerRouter.get("/getTeams", getTeams);
 
 //API to edit task Details
 managerRouter.post("/editTaskDetails", editTask);
-
-//Api to get details to edit tasks
 managerRouter.get("/getEditTadkDetails", getEditDetails);
-
-//API to get task details
 managerRouter.get("/getTaskDetails/:id", getAllTasks);
-
 managerRouter.get("/teamapi", teamdetails);
 managerRouter.get("/getManagerTaskCount", taskCount);
-
-//api to get upcoming manager tasks
 
 //api to Update Manager Profile Details
 managerRouter.post(
@@ -127,9 +113,7 @@ managerRouter.get("/getManagerProfile/:id", managerProfile);
 
 //api to get employee details
 managerRouter.get("/getEmployees", employeeData);
-
 managerRouter.delete("/removeemployeapi/:id", removeEmployee);
-// api to get manager tasks
 managerRouter.get("/getManagerTasks", managerTasks);
 
 // // api for get user,category from database
