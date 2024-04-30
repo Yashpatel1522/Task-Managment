344c2d6c05922ed22f219f6402ea51871d6e560d
YashVachhani1522 <yash.vachhani.2024@gmail.com>
2024-04-30 13:27:50 +0530

    created cards for merssge page display in cards display user image user role user name

--- controller/adminmodule/getallusers.js
+++ controller/adminmodule/getallusers.js
@@ -4,9 +4,24 @@ const logger = require("../../logger/logger");
 exports.getAllUsers = async (request, response) => {
   let db = new database();
   try {
-    response.send(
-      await db.executeQuery("select * from users where status=?", [1])
-    );
+    let res = [];
+    let users =
+      await db.executeQuery(`select up.user_id,up.newimage_name,us.first_name,us.last_name,us.role_id from users as us left join user_profiles as up on
+        us.id=up.user_id where us.status=1 order by up.create_at desc`);
+
+    for (let j = 0; j < users.length; j++) {
+      let user = users[j];
+      let flag = 0;
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].user_id == user.user_id) {
+          flag = 1;
+        }
+      }
+      if (flag == 0) {
+        res.push(user);
+      }
+    }
+    response.send(res);
   } catch (err) {
     logger.log(err);
   }
---
+++ public/js/adminmodule/displayallUsersformessage.js
@@ -0,0 +1,42 @@
+const fetchdata = async () => {
+  return await (await fetch("/admin/allusers")).json();
+};
+
+const displayAllUsers = async () => {
+  let users = await fetchdata();
+  let displayArea = document.getElementById("display-users");
+  displayArea.innerHTML = "";
+  users.forEach((user) => {
+    if (user.role_id == 1) {
+      user.role_id = "Admin";
+    } else if (user.role_id == 2) {
+      user.role_id = "Mnager";
+    } else {
+      user.role_id = "Employee";
+    }
+    displayArea.innerHTML += `<div class="container bootstrap snippets bootdey col-md-4" style="margin: 0 auto;">
+    <div class="col-md-12">
+      <!-- Begin user profile -->
+      <div class="box-info text-center user-profile-2">
+        <div class="user-profile-inner">
+          <h4 class="text-balck">${user.first_name + " " + user.last_name}</h4>
+          <img src="/assets/userprofiles/${
+            user.newimage_name
+          }" class="img-circle profile-avatar" alt="User avatar" style="border-radius: 50px;">
+          <h5>${user.role_id}</h5>
+            
+          <!-- User button -->
+          <div class="user-button">
+            <div class="row">
+              <div class="col-12">
+                <p type="button" onclick="" class="btn btn-primary btn-sm btn-block"><i class="fa fa-envelope"></i> Send Message</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+</div>`;
+  });
+};
+
