c608e8c34477333cdfe58c3a3f26baa4a9300c79
krushi0912 <krushi.patel.2024.trainee@gmail.com>
2024-05-01 14:47:58 +0530

    dynamically employee data

--- controller/managermodule/upcomingTasks.js
+++ controller/managermodule/upcomingTasks.js
@@ -3,17 +3,17 @@ const logger = require("../../logger/logger");
 let db = new database();
 const upcomingTasks = async (request, response) => {
   try {
-    let managerId = 2;
+    let managerId = request.user.id;
     let q = `select * from tasks where task_end_date between ? and ? and manager_id = ? and task_status != 'compleated' and status = 1;`;
     let res = await db.executeQuery(q, ["2019-04-25", "2024-04-30", managerId]);
 
     let teamQ = `select * from teams where created_by = ? and is_active = ?;`;
     let teamRes = await db.executeQuery(teamQ, [managerId, 0]);
 
-    return response.json({result: res, teamResult: teamRes});
+    return response.json({ result: res, teamResult: teamRes });
   } catch (error) {
     logger.error(error);
-    return response.send({error: error});
+    return response.send({ error: error });
   }
 };
 
